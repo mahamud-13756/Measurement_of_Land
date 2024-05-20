872877deb7c48c91b7a4d34e8cacb389fc4aee35
Md. Mahamud Mredha <mahamud15-13756@diu.edu.bd>
2024-05-20 16:36:01 +0600

    trinage calculation done

--- app/src/main/java/com/example/measurement_of_land/Home_page.java
+++ app/src/main/java/com/example/measurement_of_land/Home_page.java
@@ -1,12 +1,31 @@
 package com.example.measurement_of_land;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home_page extends AppCompatActivity {
+    RelativeLayout triangle;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page); // Make sure you have a corresponding home_page.xml layout file
+
+        triangle = findViewById(R.id.tri);
+
+        triangle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home_page.this, MainActivity.class));
+            }
+        });
+
+
     }
 }
