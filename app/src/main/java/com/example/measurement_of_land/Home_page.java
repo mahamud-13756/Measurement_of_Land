949423f6eb0d440dfb4b10be5d703cc80a647a3a
Md. Mahamud Mredha <mahamud15-13756@diu.edu.bd>
2024-05-20 15:54:13 +0600

    Home Page file

---
+++ app/src/main/java/com/example/measurement_of_land/Home_page.java
@@ -0,0 +1,12 @@
+package com.example.measurement_of_land;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home_page extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page); // Make sure you have a corresponding home_page.xml layout file
+    }
+}
